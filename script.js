6906482e1a531b5bdaf0419a50781f2749bccd5f
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2024-06-21 16:23:46 -0300

    treino-javascript

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 //acionar um alerta apos clicar no botão "conferir"
 
 document.getElementById('confirmButton').addEventListener('click', function() {
-  alert('Confirmação realizada com sucesso!');
+    if (document.getElementById('confirmCheckbox').checked) {
+        alert('Ação confirmada.');
+    } else {
+        alert('Ação cancelada.');
+    }
 });
+
+//conferir usuario e senha
+document.getElementById('loginButton').addEventListener('click', function() {
+    var username = document.getElementById('login').value;
+    var password = document.getElementById('senha').value;
+
+    if (username === 'admin' && password === 'password') {
+        alert('Login efetuado com sucesso.');
+    } else {
+        alert('Usuário ou senha inválidos.');
+    }
+});
+
