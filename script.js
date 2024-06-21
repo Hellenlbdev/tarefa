f626d1515574ba67e5eebf5e4922d1dee3afd5bd
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2024-06-21 16:06:05 -0300

    botão

---
+++ script.js
@@ -0,0 +1,5 @@
+//acionar um alerta apos clicar no botão "conferir"
+
+document.getElementById('confirmButton').addEventListener('click', function() {
+  alert('Confirmação realizada com sucesso!');
+});
